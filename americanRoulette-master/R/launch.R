d82114db31b631657fdc47f28507b74f92cb5833
LucaLeonardoBortolotti <luca.leonardo.bortolotti@gmail.com>
2023-04-20 18:21:12 +0200

    Added code from the repo found on github
    https://github.com/ignatkulinka/americanRoulette

---
+++ americanRoulette-master/R/launch.R
@@ -0,0 +1,10 @@
+#' Run the shiny application inside a web browser window
+#' @export
+launch <- function() {
+  shiny::runApp(system.file("shiny", package = "americanRoulette"),
+                display.mode = "normal",
+                launch.browser = TRUE)
+}
+
+
+# americanRoulette::launch()    # launch the app!
