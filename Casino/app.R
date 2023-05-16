48d29b955fca7600b8a1555dff4b1c91ffde2721
LucaLeonardoBortolotti <luca.leonardo.bortolotti@gmail.com>
2023-05-16 15:39:01 +0200

    Draft of the general structure of the app
     - Ideally, under the Roulette tabPanel, there should be 3 columns or sections:
    1) panel settings for the bets on the left
    2) roulette animation in the center
    3) roulette table on the right

--- Casino/app.R
+++ Casino/app.R
@@ -1,10 +1,23 @@
 
 # Notre app -----
 
-# I. bettingTable Setup ---------------------------------------------------
-# Setup of the bettingTable, dataframe that keeps track of slots on the table
+# Libraries ----
 require(ggplot2)
 library(shinyjs)
+library(shiny)
+require(DT)
+require(shiny)
+require(ggplot2)
+require(stringr)
+require(dplyr)
+require(data.table)
+library(tidyverse)
+
+
+# I. bettingTable Setup ---------------------------------------------------
+# Setup of the bettingTable, dataframe that keeps track of slots on the table
+
+
 
 slotNum <- c(0, "00" , c(1:36))
 
@@ -515,15 +528,6 @@ win_rate <- function(df_amount) {
 
 
 
-library(shiny)
-require(DT)
-require(shiny)
-require(ggplot2)
-require(stringr)
-require(dplyr)
-require(data.table)
-library(tidyverse)
-
 
 
   # VII. UI-----------------------------------------
@@ -532,9 +536,10 @@ library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "custome_css_style.css")
-    ),
+   tags$head(
+     tags$link(rel = "stylesheet", type = "text/css", href = "custome_css_style.css")
+     ),
+  #Curtain animation at the start
   shinyjs::useShinyjs(),
   div(
     id = "curtain",
@@ -544,82 +549,79 @@ ui <- fluidPage(
   div(
     id = "app-interface",
     headerPanel("American Roulette"),
-    fluidRow(
       # Sidebar with a slider and selection inputs
       # column to select the width
-      column(5,
-             tabsetPanel(
-               tabPanel("Betting",
-                        numericInput("startbalance", label = h3("Money Balance"), value = 1),
-                        actionButton("add", "add"),
+    navbarPage("Casino_name",
+               tabPanel("Roulette",
+                        fluidRow(
+                          column(4, style = "border: 1px solid black;",
+                                 numericInput("startbalance", label = h3("Money Balance"), value = 1),
+                                 actionButton("add", "add"),
 
-                        hr(),
-                        #fluidRow(column(3, verbatimTextOutput("money"))),
+                                 hr(),
+                                 #fluidRow(column(3, verbatimTextOutput("money"))),
 
-                        br(),
-                        ### Manual Betting
-                        h4("Manual Betting"),
-                        strong("Bet Amount:"),
-                        br(),
-                        # \10\25\50\100\250\ bet buttons
-                        actionButton("bet1", "$10"),
-                        actionButton("bet2", "$25"),
-                        actionButton("bet3", "$50"),
-                        actionButton("bet4", "$100"),
-                        actionButton("bet5", "$250"),
-                        br(),
-                        br(),
-                        h4("Chip Color"),
-                        selectizeInput("chipColor", "Choose chip color:",
-                                       choices = tolower(colors()[grepl("^[^0-9]*$", colors())]),
-                                       selected = "navy"),
-                        hr(),
+                                 br(),
+                                 ### Manual Betting
+                                 h4("Manual Betting"),
+                                 strong("Bet Amount:"),
+                                 br(),
+                                 # \10\25\50\100\250\ bet buttons
+                                 actionButton("bet1", "$10"),
+                                 actionButton("bet2", "$25"),
+                                 actionButton("bet3", "$50"),
+                                 actionButton("bet4", "$100"),
+                                 actionButton("bet5", "$250"),
+                                 br(),
+                                 br(),
+                                 h4("Chip Color"),
+                                 selectizeInput("chipColor", "Choose chip color:",
+                                                choices = tolower(colors()[grepl("^[^0-9]*$", colors())]),
+                                                selected = "navy"),
+                                 hr(),
 
-                        actionButton("spin", "Spin Roulette"),
-                        actionButton("reset", "Reset Bets"),
+                                 actionButton("spin", "Spin Roulette"),
+                                 actionButton("reset", "Reset Bets"),
 
-                        textOutput("roulette"),
+                                 textOutput("roulette"),
 
-                        # we show our balance of money
-                        textOutput("generalbalance")
+                                 # we show our balance of money
+                                 textOutput("generalbalance")
+                                 ),
+                          column(4, style = "border: 1px solid black; min-width: 80%; max-width: 100%;",
+                                 plotOutput("rTable", click = "plot_click", fill = TRUE)
+                                 ),
+                          column(4, style = "border: 1px solid black;",
+                                 textOutput("testeststststst"),
+                                 actionButton("spin", "Spin Roulette")
+                                 )
+                          )
                         ),
-      tabPanel("Statistics",
-               # Statistics inputs
-               numericInput("num_sims", "Number of simulations:", 10, min = 1),
-               numericInput("start_bet", "Balance:", 100, min = 1),
-               numericInput("bet_amount", "bet amount:", 10, min = 10),
-               numericInput("tot_spin", "Number of spins per simulation:", 50, min = 10),
-               actionButton("run_simulation", "Run simulation")
+               tabPanel("Statistics",
+                        sidebarPanel(# Statistics inputs
+                          numericInput("num_sims", "Number of simulations:", 10, min = 1),
+                          numericInput("start_bet", "Balance:", 100, min = 1),
+                          numericInput("bet_amount", "bet amount:", 10, min = 10),
+                          numericInput("tot_spin", "Number of spins per simulation:", 50, min = 10),
+                          actionButton("run_simulation", "Run simulation")),
+                        mainPanel(br(),
+                                  h4("Win Rate Percentage"),
+                                  plotOutput("win_rate_plot", height = "200px"),
+                                  br(),
+                                  plotOutput("martingale_plot", height = "400px")
+                                  )
+
+                        )
                )
-      )
-      ),
-      column(7,
-             column(12,
-                    br(),
-                    h4("Win Rate Percentage"),
-                    plotOutput("win_rate_plot", height = "200px")
-             ),
-             column(12,
-                    br(),
-                    plotOutput("martingale_plot", height = "400px")
-             ),
-
-
-        # We create other panels to the main one in order to show different things.
-        tabsetPanel(
-          tabPanel("Roulette Table", plotOutput("rTable", click = "plot_click", width = "20%")),
-          )
-
-        ))
+    )
   )
-)
 
 # VIII. Server-----------------------------------------
 
 server <- function(input, output,session) {
 
 
-  shinyjs::hide("app-interface")
+  shinyjs::hide("app-interface") #used for the curtain animation at the start
 
   # I. Reactive Data Frames -------------------------------------------------
   # Store user names and colors
@@ -804,7 +806,7 @@ server <- function(input, output,session) {
       rouletteTable
 
     }
-  }, width = 700, height = 700)
+  })
 
 
   # IV. Event Observers -----------------------------------------------------
